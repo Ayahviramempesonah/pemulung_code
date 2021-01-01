dfbfad07a2378fae87894513403ca08d0d0471cd
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2021-01-01 23:10:09 +0700

    MyconsoleTest

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	for (int i = 0 ; i <= 10; i++){
+		System.out.print(i);
+	}
         
     }
     
